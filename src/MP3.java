dd72a77a6b47a4f109b00a77788d8d0a9b9bedc9
leducminh11224188 <136835576+leducminh11224188@users.noreply.github.com>
2024-03-14 07:35:46 +0700

    Update MP3.java

--- src/MP3.java
+++ src/MP3.java
@@ -1,2 +1,16 @@
-public class MP3 {
+public class MP3 extends Item {
+  private int duration;
+
+  public MP3(String name, String description, String ID, float price, int duration){
+    super(name,description,ID,price);
+    this.duration = duration;
+  }
+
+  public int getDuration(){
+    return duration;
+  }
+
+  public void setDuration(int duration){
+    this.duration = duration;
+  }
 }
