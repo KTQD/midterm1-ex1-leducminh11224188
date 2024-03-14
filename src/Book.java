472386775af5813f559685f4ee8a377539e49d65
leducminh11224188 <136835576+leducminh11224188@users.noreply.github.com>
2024-03-14 07:42:44 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -1,2 +1,31 @@
-public class Book {
+public class Book extends Item {
+  private String author;
+  private int numberOfPages;
+  private String genre;
+
+  public Book(String name, String description, String ID, float price, String author, int numberOfPages, String genre){
+    super(name,description,ID,price);
+    this.author = author;
+    this.numberOfPages = numberOfPages;
+    this.genre = genre;
+  }
+     public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getNumberOfPages() {
+        return numberOfPages;
+    }
+
+    public void setNumberOfPages(int numberOfPages) {
+        this.numberOfPages = numberOfPages;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
 }
