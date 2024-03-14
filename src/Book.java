d41b7c8f9722a3aa7a971cc5abaa7f79a5be1aba
leducminh11224188 <136835576+leducminh11224188@users.noreply.github.com>
2024-03-14 07:52:36 +0700

    Update Book.java

--- src/Book.java
+++ src/Book.java
@@ -28,4 +28,15 @@ public class Book extends Item {
     public void setGenre(String genre) {
         this.genre = genre;
     }
+
+  @Override
+    public void showInfo() {
+        System.out.println("Name: " + name);
+        System.out.println("Description: " + getDescription());
+        System.out.println("ID: " + ID);
+        System.out.println("Price: " + price);
+        System.out.println("Author: " + author);
+        System.out.println("Number of Pages: " + numberOfPages);
+        System.out.println("Genre: " + genre);
+    }
 }
