13f7eacf61b5b9b1244c38d049b99e017befc8b1
leducminh11224188 <136835576+leducminh11224188@users.noreply.github.com>
2024-03-14 07:46:03 +0700

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        MP3 mp3;
+        MP3 mp3 = new MP3("Duc Minh's MP3", "Good bass", "1406", 9.99f, 200)
         mp3.showInfo();
 
-        Book book;
+        Book book = new Book("Math", "Book description", "0210", 9.99f, "Duc Minh", 500, "Lecture");
         book.showInfo();
 
     }
